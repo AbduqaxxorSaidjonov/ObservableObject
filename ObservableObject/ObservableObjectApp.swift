93695f65a992d803d511726370fc4264173d7f37
GitHub <abduqahhor2349@gmail.com>
2022-02-25 15:38:14 +0500

    Initial Commit

---
+++ ObservableObject/ObservableObjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  ObservableObjectApp.swift
+//  ObservableObject
+//
+//  Created by Abduqaxxor on 25/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ObservableObjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
